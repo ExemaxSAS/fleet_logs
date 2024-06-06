459e6ba3830ee35d7683ea045899c64e6c3d9de8
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2024-06-06 17:54:11 -0300

    Add changes in model, views and manifest update version 16

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/fleet_fuel.xml',
         #'views/fleet_service.xml'
     ],
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import fleet_vehicle_log_fuel
